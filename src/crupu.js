1494ce38b8e205a07624ed0a1b2595575cf44a87
HS Coetzee <hendrik@pepperbytes.net>
2016-03-29 21:41:56 +0200

    Added support for preample. Added bootstrap to sample.

--- src/crupu.js
+++ src/crupu.js
@@ -1,11 +1,17 @@
 (function () {
+
   document.addEventListener("DOMContentLoaded", function(event) {
-    var yearElements = document.getElementsByClassName('crupu.year'),
+    var crupuYearElements = document.getElementsByClassName('crupu-year'),
+    crupuPreElements = document.getElementsByClassName('crupu-pre'),
     i =0,
     fullYear = new Date ().getFullYear();
 
-    for (i = 0; i < yearElements.length; i++) {
-      yearElements[i].innerHTML = fullYear;
+    for (i = 0; i < crupuYearElements.length; i++) {
+      crupuYearElements[i].innerHTML = fullYear;
+    }
+
+    for (i = 0; i < crupuPreElements.length; i++) {
+      crupuPreElements[i].innerHTML = 'Copyright &copy;';
     }
   });
 
